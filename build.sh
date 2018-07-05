1bfa5a61039b523baabf66466cae5a95c63590dd
bgops <bgops@test1.klin-pro.com>
2018-07-05 10:19:21 -0700

    Added build

--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ go get -u github.com/hunkeelin/mtls/server
 go get -u github.com/hunkeelin/klinenv 
 go get -u github.com/hunkeelin/pki 
 go build -o SuperCA *go
-cp SuperCA $HOME/rpmbuild/SOURCES/superca-1.0/usr/bin
-ssh rpmbuild "cd $HOME/rpmbuild/SOURCES; tar -zvcf superca-1.0.tar.gz superca-1.0"
-ssh rpmbuild "rpmbuild -ba $HOME/rpmbuild/SPECS/SuperCA.spec"
-cp $HOME/rpmbuild/RPMS/x86_64/superca-1.0-1.x86_64.rpm /tmp
