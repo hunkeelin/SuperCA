a46c8553c3da985937d6bb96e372500aa30432b5
klin Puppet-managed User <klin@ec2-klin-test-1.squaretrade.com>
2019-01-03 15:06:50 -0800

    added some classification in get.go

--- v2/get.go
+++ v2/get.go
@@ -5,9 +5,11 @@ import (
 )
 
 func (c *conn) get(w http.ResponseWriter, r *http.Request) error {
-	err := c.websigncsr(w, r)
-	if err != nil {
-		return err
-	}
+    if strings.HasPrefix(r.Header.Get("content-type"), "application/x-www-form-urlencoded") {
+        err := c.websigncsr(w, r)
+        if err != nil {
+            return err
+        }
+    }
 	return nil
 }
